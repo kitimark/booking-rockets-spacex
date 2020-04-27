d1e714cf1ef4ef4a5b9b93c2b215017f6d7347cd
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-04-27 14:42:49 +0700

    Update getLatestFlight graphql

--- src/API/Graphql.hs
+++ src/API/Graphql.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 
@@ -14,17 +15,30 @@ import Data.Text
 import Data.Morpheus
 import Data.Morpheus.Document
 import Data.Morpheus.Types
+import Control.Monad.IO.Class (liftIO)
+import qualified Spacex.Rocket as R
 
-importGQLDocumentWithNamespace "schema.gql"
+importGQLDocument "schema.gql"
 
 rootResolver :: GQLRootResolver IO () Query Undefined Undefined
 rootResolver = 
   GQLRootResolver
-    { queryResolver = Query {queryHello}
+    { queryResolver = Query {hello, getLatestFlight}
     , mutationResolver = undefined
     , subscriptionResolver = undefined }
   where
-    queryHello = pure "Hello world"
+    hello = pure "Hello world"
+    getLatestFlight = liftIO $ do 
+      flight' <- R.getLatestFlight
+      let rocketRes = (R.rocket flight')
+      let rocket' = pure Rocket { 
+        rocketId = pure (R.rocketId rocketRes),
+        rocketName = pure (R.rocketName rocketRes),
+        rocketType = pure (R.rocketType rocketRes) }
+      pure Flight 
+        { flightNumber = pure (R.flightNumber flight')
+        , missionName = pure (R.missionName flight')
+        , rocket = rocket' }
 
 gqlAPI :: ByteString -> IO ByteString
 gqlAPI = interpreter rootResolver
---
+++ src/Spacex/Rocket.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Spacex.Rocket where 
+
+import Data.Aeson
+import Data.Proxy
+import Data.Text
+import Network.HTTP.Client (newManager)
+import Network.HTTP.Client.TLS (tlsManagerSettings)
+import Servant.API
+import Servant.Client
+
+data RocketResponse = RocketResponse
+  { rocketId :: Text
+  , rocketName :: Text
+  , rocketType :: Text
+  } deriving (Eq, Show)
+
+data FlightResponse = FlightResponse
+  { flightNumber :: Int
+  , missionName :: Text 
+  , rocket :: RocketResponse
+  } deriving (Eq, Show)
+
+instance FromJSON FlightResponse where
+  parseJSON = withObject "" $ \o -> do
+    fid <- o .: "flight_number"
+    name <- o .: "mission_name"
+    r <- o .: "rocket"
+    rid <- r .: "rocket_id"
+    rname <- r .: "rocket_name"
+    rtype <- r .: "rocket_type"
+    return $ FlightResponse fid name $
+      (RocketResponse rid rname rtype)
+
+type SpacexAPI = "v3" 
+  :> "launches" 
+  :> "latest"
+  :> Get '[JSON] FlightResponse
+
+spacexAPI :: Proxy SpacexAPI
+spacexAPI = Proxy
+
+queryLatestFlight :: ClientM FlightResponse
+queryLatestFlight = client spacexAPI
+
+getLatestFlight :: IO FlightResponse
+getLatestFlight = do
+  let baseUrl' = BaseUrl Https "api.spacexdata.com" 443 ""
+  manager' <- newManager tlsManagerSettings
+  Right res <- runClientM queryLatestFlight $
+    (mkClientEnv manager' baseUrl')
+  return res
