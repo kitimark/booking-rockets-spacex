155a9f332751337b3971523b107b01ac3605fa8d
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-04-30 03:15:07 +0700

    Initial getLatestFlight resovler

--- src/API/Graphql.hs
+++ src/API/Graphql.hs
@@ -11,21 +11,24 @@
 
 module API.Graphql where
 
+import Control.Monad.IO.Class
 import Data.ByteString.Lazy.Char8
 import Data.Text
 import Data.Morpheus
 import Data.Morpheus.Document
 import Data.Morpheus.Types
 import GHC.Generics
+import Spacex.Flight
 
 data Query m = Query
   { hello :: m Text
+  , getLatestFlight :: m Flight
   } deriving (Generic, GQLType)
 
 rootResolver :: GQLRootResolver IO () Query Undefined Undefined
 rootResolver = 
   GQLRootResolver
-    { queryResolver = Query {hello}
+    { queryResolver = Query {hello, getLatestFlight = resolveGetLatestFlight}
     , mutationResolver = undefined
     , subscriptionResolver = undefined }
   where
