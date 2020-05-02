533a0312e87a8cb2e32f2722f2f14ec2287da2b0
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-05-02 22:39:15 +0700

    Refactoring logindb

--- src/Data/User.hs
+++ src/Data/User.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Data.User where
 
@@ -12,27 +18,35 @@ import Web.JWT
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Text as T
 import qualified Data.Aeson as Aeson
-import Data.Either
 
-logindb :: String -> String -> IO ()
+logindb :: String -> String -> IO T.Text
 logindb user pass = do 
-    conn <- open "data.db"
-    userData <- liftIO $ queryUserByUsername user :: IO UserField
-    close conn
-    if validatePassword (B.pack $ password userData) (B.pack pass)
-        then print $ decodeAndVerifySignature (hmacSecret "secret-key") (userToken userData)
-        else print "fail"
+  conn <- open "data.db"
+  userData <- liftIO $ queryUserByUsername user :: IO UserField
+  let passwordHash' = password userData
+  close conn
+  if validatePassword (B.pack passwordHash') (B.pack pass)
+    -- then print $ decodeAndVerifySignature (hmacSecret "secret-key") (userToken userData)
+    then return $ userToken userData
+    else error "Unauthorized"
 
 userToken :: UserField -> T.Text
 userToken userData = let 
-    key = hmacSecret "secret-key"
-    cs = mempty { 
-        jti = stringOrURI $ T.pack $ username userData
-        , unregisteredClaims =  ClaimsMap {unClaimsMap = Map.fromList [("id",Aeson.Number $ fromInteger $ userid userData)
-                                                                    , ("username",Aeson.String $ T.pack $ username userData)
-                                                                    , ("password",Aeson.String $ T.pack $ password userData)]}    
+  key = hmacSecret "secret-key"
+  header = JOSEHeader 
+    { typ = Just "JWT"
+    , cty = Nothing
+    , alg = Just HS256
+    , kid = Nothing }
+  payload = Map.fromList 
+    [ ("id",Aeson.Number $ fromInteger $ userid userData)
+    , ("username",Aeson.String $ T.pack $ username userData)
+    , ("password",Aeson.String $ T.pack $ password userData) ]
+  cs = mempty 
+    { jti = stringOrURI $ T.pack $ username userData
+    , unregisteredClaims =  ClaimsMap {unClaimsMap = payload}    
     }
-    in encodeSigned key (JOSEHeader {typ = Just "JWT", cty = Nothing, alg = Just HS256, kid = Nothing}) cs
+  in encodeSigned key header cs
 
 -- -- decodeToken :: T.Text -> 
 -- decodeToken tokenText = let mJwt = decode tokenText in fmap header mJwt
