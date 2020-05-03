40b2d5bdbe374bc4a6c081ceb8510e0126e4cffa
Wasutan Kitijerapat <markdog_@hotmail.com>
2020-05-03 11:07:35 +0700

    Extract spacex datasource

--- src/Database/Spacex.hs
+++ src/Database/Spacex.hs
@@ -24,50 +24,75 @@ import Servant.Client
 import GHC.Generics
 import Foreign.Marshal.Unsafe
 import Database.Sqlite
-import Data.Types
+
+data FlightResponse = FlightResponse
+  { flightNumber :: Int
+  , missionName :: Text 
+  , rocket :: RocketResponse
+  } deriving (Generic, Eq)
+
+instance FromJSON FlightResponse where
+  parseJSON = withObject "" $ \o -> do
+    f_num <- o .: "flight_number" 
+    m_name <- o .: "mission_name"
+    rocket <- o .: "rocket"
+    return FlightResponse
+      { flightNumber = f_num 
+      , missionName = m_name 
+      , rocket = rocket }
+
+data RocketResponse = RocketResponse
+  { rocketId :: Text
+  , rocketName :: Text
+  , rocketType :: Text
+  } deriving (Generic, Eq)
+
+instance FromJSON RocketResponse where
+  parseJSON = withObject "rocket" $ \o -> do
+    r_id <- o .: "rocket_id"
+    r_name <- o .: "rocket_name"
+    r_type <- o .: "rocket_type"
+    return RocketResponse
+      { rocketId = r_id
+      , rocketName = r_name 
+      , rocketType = r_type }
 
 baseUrl' :: BaseUrl
 baseUrl' = BaseUrl Https "api.spacexdata.com" 443 "v3"
 
 type LastestFlightAPI = "launches" :> "latest"
-  :> Get '[JSON] Flight
+  :> Get '[JSON] FlightResponse
 
 latestFlightAPI :: Proxy LastestFlightAPI
 latestFlightAPI = Proxy
 
-getLatestFlight :: IO Flight
+getLatestFlight :: IO FlightResponse
 getLatestFlight = do
   manager' <- newManager tlsManagerSettings
   Right res <- runClientM client' (mkClientEnv manager' baseUrl') 
-  let flight = flightResolver res
-  return flight
+  return res
     where client' = client latestFlightAPI
 
-resolveGetLatestFlight :: IORes e Flight
-resolveGetLatestFlight = liftIO getLatestFlight
-
-type FlightsAPI = "launches" :> Get '[JSON] [Flight]
+type FlightsAPI = "launches" :> Get '[JSON] [FlightResponse]
 
 flightsAPI :: Proxy FlightsAPI
 flightsAPI = Proxy
 
-getFlights :: IO [Flight]
+getFlights :: IO [FlightResponse]
 getFlights = do
   manager' <- newManager tlsManagerSettings
   Right res <- runClientM client' (mkClientEnv manager' baseUrl')
-  let flights = map flightResolver res
-  return flights
+  return res
     where client' = client flightsAPI
 
-type FlightAPI = "launches" :> Capture "id" Int :> Get '[JSON] Flight
+type FlightAPI = "launches" :> Capture "id" Int :> Get '[JSON] FlightResponse
 
 flightAPI :: Proxy FlightAPI
 flightAPI = Proxy
 
-getFlight :: Int -> IO Flight
+getFlight :: Int -> IO FlightResponse
 getFlight id = do
   manager' <- newManager tlsManagerSettings
   Right res <- runClientM client' (mkClientEnv manager' baseUrl')
-  let flight = flightResolver res
-  return flight
+  return res
     where client' = client flightAPI id
